8167e0ee72cfdf2e24906b441938ab41d7e20dab
Leandro Pontes Berleze <leandropberleze@gmail.com>
2022-01-15 17:41:37 -0300

    feat: add path alias

---
+++ mobile/src/constants/index.ts
@@ -0,0 +1,10 @@
+export {
+  TypeColors,
+  TextColors,
+  BackgroundColors,
+  BackgroundTypeColors,
+  HeightColors,
+  WeightColors,
+} from './colors';
+export { Gradients } from './gradients';
+export { Typography, FontSize, FontWeight } from './typography';
