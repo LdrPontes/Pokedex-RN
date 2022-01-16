35e617dd6ea6e137fb7e50860b5621e155b8cb6f
Leandro Pontes Berleze <leandropberleze@gmail.com>
2022-01-16 15:31:39 -0300

    feat: add PDXSearchField atom

--- mobile/src/app/auth/screens/SignIn/index.tsx
+++ mobile/src/app/auth/screens/SignIn/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { ButtonType, PDXButton } from '_atoms/Button';
+import { PDXSearchField } from '_atoms/SearchField';
 import { BackgroundColors } from '_constants';
 
 const SignInScreen = () => {
   return (
     <View style={styles.container}>
+      <PDXSearchField
+        onChange={value => console.log(value)}
+        placeholder="Search"
+        initial="Test"
+      />
       <PDXButton
         label="PRESS ME"
         type={ButtonType.PRIMARY}
-        onPress={() => console.log('Foi')}
+        onPress={() => console.log('Pressed')}
       />
     </View>
   );
---
+++ mobile/src/components/atoms/SearchField/index.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+
+import { TextInput, StyleSheet, View } from 'react-native';
+import {
+  BackgroundColors,
+  FontSize,
+  FontWeight,
+  TextColors,
+  TypeColors,
+  Typography,
+} from '_constants';
+import Search from '_assets/icons/search.svg';
+
+type Props = {
+  initial?: string;
+  placeholder: string;
+  onChange: (value: string) => void;
+};
+
+export const PDXSearchField: React.FC<Props> = ({
+  initial,
+  placeholder,
+  onChange,
+}) => {
+  const [isFocused, setFocus] = useState(false);
+
+  const containerStyle = {
+    ...styles.container,
+    ...(isFocused ? styles.focus : styles.default),
+  };
+
+  const onFocus = () => {
+    setFocus(true);
+  };
+
+  const onBlur = () => {
+    setFocus(false);
+  };
+
+  return (
+    <View style={containerStyle}>
+      <Search fill={TextColors.GREY} />
+      <TextInput
+        style={styles.textInput}
+        defaultValue={initial}
+        placeholder={placeholder}
+        placeholderTextColor={TextColors.GREY}
+        selectionColor={TypeColors.PSYCHIC}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        onChangeText={onChange}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 10,
+    paddingHorizontal: 28,
+  },
+  default: {
+    backgroundColor: BackgroundColors.DEFAULT_INPUT,
+  },
+  focus: {
+    backgroundColor: BackgroundColors.PRESSED_INPUT,
+  },
+  textInput: {
+    flex: 1,
+    height: 60,
+    lineHeight: 19,
+    paddingLeft: 12,
+    fontSize: FontSize.FONT_SIZE_16,
+    fontWeight: FontWeight.FONT_WEIGHT_NORMAL,
+    fontFamily: Typography.FONT_FAMILY_REGULAR,
+  },
+});
